3fa2093f2c88f78e0ad1d212127020b097071158
Ru <rudikraeher@gmail.com>
2020-12-11 12:07:40 -0800

    connect to atlas

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/Budget",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
 
 // routes
 app.use(require("./routes/api.js"));
